271e1d3e74909097ffb5cdd9213581ef6c668ba8
cohyun <syrup91@ewhain.net>
2022-05-18 20:09:10 +0900

    third commit modified layout

--- src/screens/Project.js
+++ src/screens/Project.js
@@ -1,102 +1,231 @@
-import React, { useState } from 'react'
-import '../script.js';
-import '../components/Styles/style.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button} from "react-bootstrap";
+import React, { useState } from "react";
+import "../script.js";
+import "../components/Styles/style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
 
-  const yellow=require('../assets/img/body.png')
-  const blue =require('../assets/img/body2.png')
-  const red = require('../assets/img/body3.png')
-  const green =require('../assets/img/body4.png')
-  const body = {yellow,blue,red,green}
+const Body_Yellow = require("../assets/img/body/Body_Yellow.png");
+const Body_Blue = require("../assets/img/body/Body_Blue.png");
+const Body_Red = require("../assets/img/body/Body_Red.png");
+const Body_White = require("../assets/img/body/Body_White.png");
+const body = { Body_Yellow, Body_Blue, Body_Red, Body_White };
 
-  const Bhair=require('../assets/img/hair/hair1.png')
-  const Yhair=require('../assets/img/hair/hair2.png')
-  const Blhair=require('../assets/img/hair/hair3.png')
-  const Ghair=require('../assets/img/hair/hair4.png')
-  const hair={Bhair,Yhair,Blhair,Ghair}
+const Hair_Long = require("../assets/img/hair/Hair_Long.png");
+const Hair_Puka = require("../assets/img/hair/Hair_Puka.png");
+const Hair_Short_green = require("../assets/img/hair/Hair_Short_Green.png");
+const Hair_Short = require("../assets/img/hair/Hair_Short.png");
+const hair = { Hair_Long, Hair_Puka, Hair_Short_green, Hair_Short };
 
 const Project = () => {
-const [select,setSelect]=useState(body.yellow);
-const [selecthair,setSelectHair]=useState(body.Blhair);
-const [visible, setVisible] = React.useState(true);
-const [hvisible, sethVisible] = React.useState(false);
-  
+  const [select, setSelect] = useState(body.Body_Yellow);
+  const [selecthair, setSelectHair] = useState(body.Hair_Short_green);
+  const [visible, setVisible] = React.useState(true);
+  const [hvisible, sethVisible] = React.useState(false);
+
   return (
-    //전체 div
     <div>
-
-      
-      <div class="Character-clothes"onClick={()=>{
-       setVisible(true);
-       sethVisible(false);
-     }}> 
-        <Button size="lg" variant="secondary" style={{float:'right',width:'50%',height:'50%'}}>body</Button>
+      {/* ㅣㅣㅣㅣ오른쪽 버튼ㅣㅣㅣㅣㅣㅣㅣ */}
+      <div
+        class="Character-clothes"
+        onClick={() => {
+          setVisible(true);
+          sethVisible(false);
+        }}
+      >
+        <Button
+          size="lg"
+          variant="secondary"
+          style={{ float: "right", width: "50%", height: "50%" }}
+        >
+          body
+        </Button>
+      </div>
+      <div
+        class="Character-clothes-1"
+        onClick={() => {
+          setVisible(false);
+          sethVisible(true);
+        }}
+      >
+        <Button
+          size="lg"
+          variant="dark"
+          style={{ float: "right", width: "50%", height: "50%" }}
+        >
+          hair
+        </Button>
+      </div>
+      <div class="Character-clothes-2" onClick={() => {}}>
+        <Button
+          size="lg"
+          variant="primary"
+          style={{ float: "right", width: "50%", height: "50%" }}
+        >
+          eye
+        </Button>
+      </div>
+      <div class="Character-clothes-3" onClick={() => {}}>
+        <Button
+          size="lg"
+          variant="primary"
+          style={{ float: "right", width: "50%", height: "50%" }}
+        >
+          mouth
+        </Button>
+      </div>
+      <div class="Character-clothes-4" onClick={() => {}}>
+        <Button
+          size="lg"
+          variant="primary"
+          style={{ float: "right", width: "50%", height: "50%" }}
+        >
+          egg
+        </Button>
+      </div>
+      <div class="Character-clothes-5" onClick={() => {}}>
+        <Button
+          size="lg"
+          variant="primary"
+          style={{ float: "right", width: "50%", height: "50%" }}
+        >
+          idol
+        </Button>
+      </div>
+      <div class="Character-clothes-6" onClick={() => {}}>
+        <Button
+          size="lg"
+          variant="primary"
+          style={{ float: "right", width: "50%", height: "50%" }}
+        >
+          back
+        </Button>
       </div>
 
-
-      <div class="Character-clothes-1" onClick={()=>{
-        setVisible(false);
-        sethVisible(true); 
-     }}>
-         <Button size="lg" variant="dark"style={{float:'right',width:'50%',height:'50%'}}>hair</Button>
+      {/* ㅣㅣㅣㅣ캐릭터ㅣㅣㅣㅣㅣㅣㅣ */}
+      <div class="main-Div">
+        {/* ㅣㅣㅣㅣbodyㅣㅣㅣㅣㅣㅣㅣ */}
+        <div className="Character">
+          <img
+            src={select}
+            id="chara-img"
+            style={{ width: "100%", height: "100%", objectFit: "contain" }}
+          />
+        </div>
+        {/* ㅣㅣㅣㅣhairㅣㅣㅣㅣㅣㅣㅣ */}
+        <div className="Character-1">
+          <img
+            src={selecthair}
+            id="chara-img"
+            style={{ width: "100%", height: "100%", objectFit: "contain" }}
+          />
+        </div>
+        {/* ㅣㅣㅣㅣ우측패널ㅣㅣㅣㅣㅣㅣㅣ */}
+        {/* ㅣㅣㅣㅣbodyPanelㅣㅣㅣㅣㅣㅣㅣ */}
+        {visible && (
+          <div class="RightPanel">
+            <div>
+              <img
+                src={body.Body_Red}
+                class="Card"
+                onClick={() => {
+                  setSelect(body.Body_Red);
+                }}
+              />
+              <img
+                src={body.Body_White}
+                class="Card"
+                onClick={() => {
+                  setSelect(body.Body_White);
+                }}
+              />
+            </div>
+            <div>
+              <img
+                src={body.Body_Blue}
+                class="Card"
+                onClick={() => {
+                  setSelect(body.Body_Blue);
+                }}
+              />
+              <img
+                src={body.Body_Yellow}
+                class="Card"
+                onClick={() => {
+                  setSelect(body.Body_Yellow);
+                }}
+              />
+            </div>
+          </div>
+        )}
+        {/* ㅣㅣㅣㅣhairPanelㅣㅣㅣㅣㅣㅣㅣ */}
+        {hvisible && (
+          <div class="RightPanel">
+            <div>
+              <img
+                src={body.Body_Red}
+                class="Card"
+                onClick={() => {
+                  setSelect(body.Body_Red);
+                }}
+              />
+              <img
+                src={body.Body_White}
+                class="Card"
+                onClick={() => {
+                  setSelect(body.Body_White);
+                }}
+              />
+            </div>
+            <div>
+              <img
+                src={body.Body_Blue}
+                class="Card"
+                onClick={() => {
+                  setSelect(body.Body_Blue);
+                }}
+              />
+              <img
+                src={body.Body_Yellow}
+                class="Card"
+                onClick={() => {
+                  setSelect(body.Body_Yellow);
+                }}
+              />
+            </div>
+          </div>
+        )}
       </div>
 
-      <div class="Character-clothes-2" onClick={()=>{
-       console.log("blue")   
-     }}>
-        <Button size="lg" variant="primary"style={{float:'right',width:'50%',height:'50%'}}>egg</Button>
-      </div>
-      
-<div class="main-Div">
+      {/* {hvisible && (
+        <div class="Character-color button">
+          <button
+            class="change-color-black"
+            onClick={() => {
+              setSelectHair(hair.Hair_Long);
+            }}
+          ></button>
+          <button
+            class="change-color-Body_Blue"
+            onClick={() => {
+              setSelectHair(hair.Hair_Short_green);
+            }}
+          ></button>
+          <button
+            class="change-color-white"
+            onClick={() => {
+              setSelectHair(hair.Hair_Short);
+            }}
+          ></button>
+          <button
+            class="change-color-Body_Yellow"
+            onClick={() => {
+              setSelectHair(hair.Hair_Puka);
+            }}
+          ></button>
+        </div>
+      )} */}
+    </div>
+  );
+};
 
-<div className="Character">
-  <img src={select}  id="chara-img" style={{ width:'100%',height:'100%', objectFit:'contain'}}/>   
-   </div>
-
-   <div className='Character-1' >
-   <img src={selecthair} id="chara-img" style={{ width:'100%',height:'100%', objectFit:'contain'}}/>
-   </div>
-   {visible  && <div class="Character-color button">
-   
-   <button class="change-color-yellow" onClick={()=>{
-       setSelect(body.yellow)       
-     }}></button>
-   <button class="change-color-blue" onClick={ ()=>{
-       setSelect(body.blue)
-     }}></button>
-     <button class="change-color-red" onClick={()=>{
-       setSelect(body.red)
-     }}></button>
-     <button class="change-color-green" onClick={()=>{
-        setSelect(body.green)
-     }}></button>
-     
-</div>}
-
-
-{hvisible  && <div class="Character-color button">
-   
-   <button class="change-color-black" onClick={()=>{
-       setSelectHair(hair.Bhair)       
-     }}></button>
-   <button class="change-color-blue" onClick={ ()=>{
-       setSelectHair(hair.Blhair)
-     }}></button>
-     <button class="change-color-green" onClick={()=>{
-       setSelectHair(hair.Ghair)
-     }}></button>
-     <button class="change-color-yellow" onClick={()=>{
-        setSelectHair(hair.Yhair)
-     }}></button>
-     
-</div>}
-
-      
-      
-</div>
-</div>
-  )
-}
-
-export default Project
+export default Project;
--- src/script.js
+++ src/script.js
@@ -1,9 +1,4 @@
 
-export function body(){
-    return(
-        <div></div>
-    )
-}
 
 
 export function changeChcolor (color){
