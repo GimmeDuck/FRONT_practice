06f2b7dc5656ae4bff4302a09f5939004c0a607d
cohyun <syrup91@ewhain.net>
2022-05-20 22:27:57 +0900

    opensea 연결

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const about = () => {
   return (
-   <div><h1>About</h1></div>
-  )
-}
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <div>
+        <h1>About</h1>
+      </div>
+      <iframe
+        src="https://opensea.io/collection/moonbears-solana?embed=true"
+        width="100%"
+        height="100%"
+        frameborder="0"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
+};
 
-export default about
+export default about;
--- src/screens/Project.js
+++ src/screens/Project.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "../script.js";
 import "../components/Styles/style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button } from "react-bootstrap";
